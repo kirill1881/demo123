d44886b2ab688a5421b77eb3fcca7fd40b58fadd
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-09-11 12:32:04 +0300

    123

---
+++ src/main/java/com/example/demo123/helpers/DetailModel.java
@@ -0,0 +1,11 @@
+package com.example.demo123.helpers;
+
+import com.example.demo123.models.ItemModel;
+import lombok.Data;
+
+@Data
+public class DetailModel {
+    private ItemModel itemModel;
+    private String usdPrise;
+    private String eurPrice;
+}
